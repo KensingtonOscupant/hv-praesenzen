f8c2077d1d0ebe5165c4959fd3b0378b05b32648
Felix Ringe <felix.ringe@fu-berlin.de>
2023-12-21 00:52:03 +0100

    refactored and cleaned code

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def load_config(config_file):
 config = load_config('config.json')
 
 # Load the CSV file (test data or full data)
-df = pd.read_csv(config['csv_file'])
+df = pd.read_csv(config['csv_file_path'])
 
 # Set the base directory for where the folders with the PDFs are located
 directory = config['directory']
@@ -108,6 +108,36 @@ def process_pdf(file_path):
     except Exception as e:
         return None, str(e)
 
+def call_language_model(client, system_prompt, user_prompt, full_text, calculate_cost):
+    """
+    Makes a call to the GPT language model and returns the response and the cost.
+
+    Parameters:
+    client (obj): The GPT client object.
+    system_prompt (str): The system prompt for the GPT model.
+    user_prompt (str): The user prompt for the GPT model.
+    full_text (str): The text to be analyzed by the GPT model.
+    calculate_cost (function): Function to calculate the cost of the GPT call.
+
+    Returns:
+    tuple: A tuple containing the response from GPT and the cost of the call.
+    """
+    combined_prompt = user_prompt + full_text
+    response = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": combined_prompt}
+        ],
+        temperature=0.2
+    )
+
+    input_tokens = response.usage.prompt_tokens
+    output_tokens = response.usage.completion_tokens
+    cost = calculate_cost(input_tokens, output_tokens)
+
+    return response.choices[0].message.content, cost
+
 # log a row of data to the W&B table
 def log_to_wandb(data):
     table.add_data(*data.values())
@@ -133,10 +163,8 @@ for index, row in tqdm(df.iloc[start_index:end_index].iterrows(), total=total_ro
         "Presence_predicted": None,
         "correct": None,
         "error": None,
-        "price": 0,
+        "cost": 0,
         "file_path": None,
-        "standard_deviation": None,
-        "mean": None,
         "comment": ""
     }
 
@@ -152,7 +180,7 @@ for index, row in tqdm(df.iloc[start_index:end_index].iterrows(), total=total_ro
     alternative_document_structure = False
 
     # total cost pf processing the document
-    cost_total = 0
+    cost = 0
 
     # standard deviation of the highest percentage of each document to the other values for the same document
     std_dev = 0
@@ -203,61 +231,40 @@ for index, row in tqdm(df.iloc[start_index:end_index].iterrows(), total=total_ro
     print(system_prompt)
     user_prompt = "Im folgenden erhältst du einen Bericht einer Hauptversammlung. Das Dokument enthält eine Tabelle mit einer Kopfzeile, aber die Kopfzeile ist beim Extrahieren des Texts beschädigt worden. Bitte gib mir die volle Bezeichnung jeder Spalte in der korrekten Reihenfolge, wie sie im Dokument auftaucht. Antworte ausschließlich mit einer Liste im Format [spalte_1, spalte_2, spalte_3]. Es kann auch eine Kopfzeile mit Multi-Index sein. Wenn du keine Kopfzeile finden kannst, antworte mit [0]. Bericht: "
     print(user_prompt)
-    combined_prompt = user_prompt + full_text
 
     header_row_list = []
 
-    # make the GPT call 5 times
     for i in range(1):
+        response, cost = call_language_model(client, system_prompt, user_prompt, full_text, calculate_cost)
+        header_row_list.append(response)
+        data['cost'] += cost
 
-        # Call the GPT-4 chat_completion model
-        response = client.chat.completions.create(model="gpt-4-1106-preview",  # Specify the model, e.g., "gpt-4"
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": combined_prompt}
-        ],
-        temperature=0.2)
+    header_counts = Counter(header_row_list)
 
-        # calculate cost
-        input_tokens = response.usage.prompt_tokens
-        output_tokens = response.usage.completion_tokens
-        cost_header_detection = calculate_cost(input_tokens, output_tokens)
+    most_common_count = header_counts.most_common(1)[0][1]  # Get the count of the most common string
 
-        # header_row_list.append(response.choices[0].message.content)
+    # Find all strings that have this highest count
+    most_common_headers = [header for header, count in header_counts.items() if count == most_common_count]
 
-        # cost_total += cost_header_detection
-
-    most_common_header = response.choices[0].message.content
-
-    # header_counts = Counter(header_row_list)
-
-    # most_common_count = header_counts.most_common(1)[0][1]  # Get the count of the most common string
-
-    # # Find all strings that have this highest count
-    # most_common_headers = [header for header, count in header_counts.items() if count == most_common_count]
-
-    # # # Handle the situation based on the number of most common strings
-    # if len(most_common_headers) == 1:
-    #     # Only one most common string
-    #     most_common_header = most_common_headers[0]
-    #     print("Most common header:", most_common_header)
-    # elif len(most_common_headers) > 1:
-    #     # Multiple strings with the same highest frequency
-    #     # Handle this situation as you see fit
-    #     print("Multiple headers are equally common:", most_common_headers)
-    #     error = True
-    #     comment_collection += "Mehrere Kopfzeilen sind gleich häufig"
-    #     table.add_data(id_value, year_value, row['Presence_enhanced'], 0, None, error, cost_total, file_path, std_dev, mean, comment_collection)
-    #     continue
-    #     # For example, you can choose to use the first one, or handle it as an error, etc.
-    #     # most_common_header = most_common_headers[0]  # Example: Use the first one
-    # else:
-    #     # No headers found (if your list could be empty)
-    #     error = True
-    #     comment_collection += "Etwas ist grundsätzlich beim Finden der Kopfzeile schief gelaufen"
-    #     table.add_data(id_value, year_value, row['Presence_enhanced'], 0, None, error, cost_total, file_path, std_dev, mean, comment_collection)
-    #     print("No headers found")
-    #     continue
+    # # Handle the situation based on the number of most common strings
+    if len(most_common_headers) == 1:
+        # Only one most common string
+        most_common_header = most_common_headers[0]
+        print("Most common header:", most_common_header)
+    elif len(most_common_headers) > 1:
+        # Multiple strings with the same highest frequency
+        print("Multiple headers are equally common:", most_common_headers)
+        data.update({
+            "error": "Multiple common headers",
+            "comment": most_common_headers
+        })
+        log_to_wandb(data)
+        continue
+    else:
+        data.update({
+            "error": "No header row found"
+        })
+        continue
 
     # Regular expression pattern to check if string starts with '[' and ends with ']'
     pattern = r'^\[.*\]$'
@@ -272,169 +279,103 @@ for index, row in tqdm(df.iloc[start_index:end_index].iterrows(), total=total_ro
         if column_names == "[0]":
 
             user_prompt = "Im folgenden erhältst du einen Bericht einer Hauptversammlung. Werden in dem Dokument wiederholt und ausdrücklich Angaben zum auf der Versammlung vertretenen Grundkapital in Prozent gemacht (Also bspw. 'Grundkapital: 30%'? Antworte nur mit [1] oder [0]. Bericht: "
-            print(user_prompt)
-            combined_prompt = user_prompt + full_text
 
-            # Call the GPT-4 chat_completion model
-            response = client.chat.completions.create(model="gpt-4-1106-preview",  # Specify the model, e.g., "gpt-4"
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": combined_prompt}
-            ],
-            temperature=0.2)
-
-            print(response.choices[0].message.content)
-
-            # calculate cost
-            input_tokens = response.usage.prompt_tokens
-            output_tokens = response.usage.completion_tokens
-            cost_alternative_doc_structure_detection = calculate_cost(input_tokens, output_tokens)
-
-            cost_total += cost_alternative_doc_structure_detection
+            response, cost = call_language_model(client, system_prompt, user_prompt, full_text, calculate_cost)
+            header_row_list.append(response)
+            data['cost'] += cost
 
             # check response
-            if response.choices[0].message.content == "[1]":
+            if response == "[1]":
 
                 alternative_document_structure = True
 
                 # find the highest grundkapital percentage in the document
                 user_prompt = "Im folgenden erhältst du einen Bericht von einer Hauptversammlung. Antworte ausschließlich mit einer Liste im Format [zahl_1, zahl_2, zahl_3], die ausschließlich alle die genannten Prozentzahlen enthält, die sich auf den Prozentsatz des auf der Hauptversammlung vertretenen Grundkapitals beziehen. Wenn du dir nicht absolut sicher bist, antworte mit [0]. Verwende Punkt statt Komma für die Zahlen. Bericht: "
-                print(user_prompt)
-                combined_prompt = user_prompt + full_text
 
-                # Call the GPT-4 chat_completion model
-                response = client.chat.completions.create(model="gpt-4-1106-preview",  # Specify the model, e.g., "gpt-4"
-                messages=[
-                    {"role": "system", "content": system_prompt},
-                    {"role": "user", "content": combined_prompt}
-                ],
-                temperature=0.2)
+                response, cost = call_language_model(client, system_prompt, user_prompt, full_text, calculate_cost)
+                header_row_list.append(response)
+                data['cost'] += cost
 
-                print(response.choices[0].message.content)
-
-                # calculate cost
-                input_tokens = response.usage.prompt_tokens
-                output_tokens = response.usage.completion_tokens
-                cost_alternative_doc_structure_extraction = calculate_cost(input_tokens, output_tokens)
-
-                cost_total += cost_alternative_doc_structure_extraction
-
-            elif response.choices[0].message.content == "[0]":
-                comment_collection += "Keine Kopfzeile gefunden"
-                error = True
-                table.add_data(id_value, year_value, row['Presence_enhanced'], 0, None, error, cost_total, file_path, std_dev, mean, comment_collection)
-                error_during_page_assessment = True
+            elif response == "[0]":
+                data.update({
+                    "error": "Header row not found",
+                })
+                log_to_wandb(data)
                 continue
 
             else:
-                comment_collection += response.choices[0].message.content + "\n"
-                error = True
-                table.add_data(id_value, year_value, row['Presence_enhanced'], 0, None, error, cost_total, file_path, std_dev, mean, comment_collection)
-                error_during_page_assessment = True
+                data.update({
+                    "error": "Unexpected response from GPT model to the question about alternative document structure",
+                    "comment": response,
+                })
+                log_to_wandb(data)
                 continue
 
     else:
-        # Handle the case where the string doesn't match
-        print("String does not start with '[' and end with ']'")
-        comment_collection += response.choices[0].message.content + "\n"
-        error = True
-        table.add_data(id_value, year_value, row['Presence_enhanced'], 0, None, error, cost_total, file_path, std_dev, mean, comment_collection)
-        error_during_page_assessment = True
-        break
+        data.update({
+            "error": "Unexpected response from GPT model to the question about the header row",
+            "comment": response,
+        })
+        log_to_wandb(data)
+        continue
 
     if alternative_document_structure == False:
         user_prompt = "Im folgenden erhältst du einen Bericht einer Hauptversammlung. Das Dokument enthält eine Tabelle mit einer Kopfzeile, aber die Kopfzeile ist beim Extrahieren des Texts beschädigt worden. Die korrekte Kopfzeile habe ich angehängt. Antworte ausschließlich mit einer Liste im Format [zahl_1, zahl_2, zahl_3], die ausschließlich alle die genannten Prozentzahlen enthält, die sich auf den Prozentsatz des auf der Hauptversammlung vertretenen Grundkapitals beziehen. Durchsuche das ganze Dokument nach solchen Zahlen, auch außerhalb von Tabellen. Wenn du dir nicht absolut sicher bist, antworte mit [0]. Verwende Punkt statt Komma für die Zahlen. \n Korrekte Kopfzeile: "
-        print("user_prompt: ", user_prompt)
-        combined_prompt = user_prompt + column_names + "\n Bericht der Hauptversammlung: " + full_text
+        print(user_prompt)
+        user_prompt += column_names + "\n Bericht der Hauptversammlung: "
 
-        # Call the GPT-4 chat_completion model
-        response = client.chat.completions.create(model="gpt-4-1106-preview",  # Specify the model, e.g., "gpt-4"
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": combined_prompt}
-        ],
-        temperature=0.2)
+        response, cost = call_language_model(client, system_prompt, user_prompt, full_text, calculate_cost)
+        header_row_list.append(response)
+        data['cost'] += cost
 
-        # calculate cost
-        input_tokens = response.usage.prompt_tokens
-        output_tokens = response.usage.completion_tokens
-        cost_info_extraction = calculate_cost(input_tokens, output_tokens)
-
-        cost_total += cost_info_extraction
-
-        # Print the response
-        print(response.choices[0].message.content)
-
-    # get highest value within page
     try:
-        percentage_list = ast.literal_eval(response.choices[0].message.content)
-        highest_percentage_of_page = max(percentage_list)
-    except SyntaxError:
-        highest_percentage_of_page = 0
-        comment_collection += response.choices[0].message.content + "\n"
-        error = True
-
-    # get highest value of all pages
-    try:
-        # highest_percentage_list.append(percentage_intro)
-        highest_percentage_list.append(highest_percentage_of_page)
-        highest_percentage = max(highest_percentage_list)
-        # round highest percentage to 2 decimal places
-        highest_percentage = round(highest_percentage, 2)
-    except ValueError:
-        highest_percentage = 0
-        comment_collection += response.choices[0].message.content + "\n"
-        error = True
-
-
-    if error == False:
-        # Convert the percentages to a numpy array for statistical calculations
-        percentages = np.array(highest_percentage_list)
-
-        # Calculate mean and standard deviation
-        mean = np.mean(percentages)
-        std_dev = np.std(percentages)
-
-        # check if highest_percentage is more than 15 away from the second highest percentage
-        if len(highest_percentage_list) > 1:
-            second_highest_percentage = sorted(highest_percentage_list, reverse=True)[1]
-            if highest_percentage - second_highest_percentage > 10:
-                comment_collection += "Der ermittelte Wert weicht um mehr als 10 Prozentpunkte vom zweithöchsten Wert ab."
-                error = True
-
-        if error:
-            table.add_data(id_value, year_value, row['Presence_enhanced'], highest_percentage, None, error, cost_total, file_path, std_dev, mean, comment_collection)
-            continue
-
-    print("highest_percentage: ", highest_percentage)
-
-    # calculate cost
-    input_tokens = response.usage.prompt_tokens
-    output_tokens = response.usage.completion_tokens
-    cost_page_analysis = calculate_cost(input_tokens, output_tokens)
-
-    cost_total += cost_page_analysis
-
-    if error_during_page_assessment:
+        # Parse percentage list from response for the entire document
+        percentage_list = ast.literal_eval(response)
+        if percentage_list:
+            highest_percentage = max(percentage_list)
+            data['Presence_predicted'] = round(highest_percentage, 2)
+    except (SyntaxError, ValueError) as e:
+        data["comment"] += f"{response} "
+        data.update({
+            "error": "Error parsing list of percentages"
+        })
+        log_to_wandb(data)
         continue
 
-    df.at[index, 'Presence_predicted'] = highest_percentage
+    # sanity checks
 
-    if row['Presence_enhanced'] == highest_percentage:
-        prediction_correct = True
+    if len(highest_percentage_list) > 1:
+        second_highest_percentage = sorted(highest_percentage_list, reverse=True)[1]
+        if highest_percentage - second_highest_percentage > 10:
+            data["comment"] += "Der ermittelte Wert weicht um mehr als 10 Prozentpunkte vom zweithöchsten Wert ab. "
+            data.update({
+                "error": "Significant difference from second highest value",
+                "Presence_predicted": highest_percentage
+            })
+            log_to_wandb(data)
+            continue
+
+    print("highest_percentage: ", data['Presence_predicted'])
+
+    if data['Presence_enhanced'] == data['Presence_predicted']:
+        data['correct'] = True
     else:
         # Check for another row with the same ID_Key_original and Year_original
         same_id_year_rows = df[(df['ID_Key_original'] == row['ID_Key_original']) & 
                             (df['Year_original'] == row['Year_original'])]
 
         # Check if any of those rows have Presence_enhanced equal to highest_percentage
-        if any(same_id_year_rows['Presence_enhanced'] == highest_percentage):
-            prediction_correct = True
-            comment_collection += "Der ermittelte Wert stammt aus dem anderen Bericht diesen Jahres und ist dort korrekt ermittelt."
+        if any(same_id_year_rows['Presence_enhanced'] == data['Presence_predicted']):
+            data['correct'] = True
+            data["comment"] += "Der ermittelte Wert stammt aus dem anderen Bericht diesen Jahres und ist dort korrekt ermittelt. "
         else:
-            prediction_correct = False
+            data['correct'] = False
 
-    table.add_data(id_value, year_value, row['Presence_enhanced'], highest_percentage, prediction_correct, error, cost_total, file_path, std_dev, mean, comment_collection)
+    # Log the data
+    log_to_wandb(data)
 
 print("system_prompt: ", system_prompt)
 print("user_prompt: ", user_prompt)
+
+# Save the table to W&B
+wandb.log({"results": table})
