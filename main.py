d018c76f4f3e58f09bbef2f273083b78783a19a6
KensingtonOscupant <felix.ringe@fu-berlin.de>
2025-07-31 23:39:38 +0200

    uncluttered and settled on prompt versioning approach

--- main.py
+++ main.py
@@ -6,35 +6,32 @@ from pydantic import BaseModel
 import asyncio
 import json
 from utilities import process_pdf
+from openai_prices import price
+from prompts.registry import Prompt  
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Load the config file
-def load_config(config_file):
-    with open(config_file, 'r') as file:
-        return json.load(file)
+client = weave.init("agm_minimum_share_capital_present_14")
 
-config = load_config('config.json')
-
-client = weave.init("agm_share_capital_present_13")
-
-llm_name = os.getenv("LLM_NAME")
 split = os.getenv("SPLIT")
-prompt_name = os.getenv("PROMPT_NAME")
+llm_name = os.getenv("LLM_NAME")
+price_per_1m_tokens = price(llm_name)
+prompt_setup = os.getenv("PROMPT_SETUP")
+description = os.getenv("MODEL_DESCRIPTION")
 
-# create prompt objects
-system_prompt = weave.StringPrompt(config['prompts']['system_prompt'])
-single_label_value_prompt = weave.StringPrompt(config['prompts']['user_prompts']['single_label_value_prompt'])
+system_prompt_ref = Prompt.SYSTEM.ensure_ref()
+base_prompt_ref = Prompt.BASE.ensure_ref()
 
-#publish for version control
-weave.publish(system_prompt, name="system_prompt")
-weave.publish(single_label_value_prompt, name="single_label_value_prompt")
+# name of the run that appears on the wandb leaderboard
+leaderboard_name = f"{llm_name}_using_{prompt_setup}"
 
 class LabelValueOutput(BaseModel):
     label_value_llm_output: float
 
 class AGMPresenceModel(Model):
+    system_prompt: weave.trace.refs.ObjectRef
+    base_prompt: weave.trace.refs.ObjectRef
 
     @weave.op()
     def predict(self, file_path: str):
@@ -50,18 +47,30 @@ class AGMPresenceModel(Model):
         label_value_response = client.responses.parse(
             model=llm_name, 
             input=[
-                {"role": "system", "content": system_prompt.content},
-                {"role": "user", "content": single_label_value_prompt.content.format(
+                {"role": "system", "content": system_prompt_ref.get().content},
+                {"role": "user", "content": base_prompt_ref.get().content.format(
                     report=text
                 )}
             ],
             text_format=LabelValueOutput
         )
 
-        return {'label_value_predicted': round(label_value_response.output_parsed.label_value_llm_output, 2),
-                'metadata': metadata}
+        # helper for cost tracking; currently doesn't work with this setup
+        # due to wandb-acknowledged bug
+        u = label_value_response.usage
+        cost = (u.input_tokens * price_per_1m_tokens["input"] / 1_000_000
+                + u.output_tokens * (price_per_1m_tokens["output"] / 1_000_000))
 
-model = AGMPresenceModel(name="single_prompt_o3_mini", description="Plain attempt with just a single prompt, no few-shot examples.")
+        if label_value_response.output_parsed.label_value_llm_output == -2:
+            label_value_predicted = None
+        else:     
+            label_value_predicted = round(label_value_response.output_parsed.label_value_llm_output, 2)
+
+        return {'label_value_predicted': label_value_predicted,
+                'metadata': metadata,
+                'cost': cost}
+
+model = AGMPresenceModel(name=leaderboard_name, description=description, system_prompt=system_prompt_ref, base_prompt=base_prompt_ref)
 
 eval = weave.ref(f"{split}_eval").get()
 
