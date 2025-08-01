a79573795cf60c648605bb3a41c56b391186aaaa
KensingtonOscupant <felix.ringe@fu-berlin.de>
2025-08-01 12:37:02 +0200

    moved some more config into env vars

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ import os
 load_dotenv()
 
 project_name = os.getenv("PROJECT_NAME")
+dataset_name = os.getenv("DATASET_NAME")
 
 weave.init(project_name)
 
 for split in ["train", "dev", "test"]:
 
     # upload dataset
-    csv_path = f"data/250730_{split}_set.csv"
+    csv_path = f"data/{dataset_name}_{split}_set.csv"
     df = pd.read_csv(csv_path)
     dataset_object = Dataset.from_pandas(df)
     dataset_ref = weave.publish(dataset_object, name=f"{split}_set") # at least i think it's a ref no?
