721c10f0dc30114f8c33c5f12fb28f1001a57422
KensingtonOscupant <felix.ringe@fu-berlin.de>
2025-08-02 07:56:07 +0200

    renamed main.py to eval.py and moved the model into a separate module to be able to use it in prod

---
+++ model.py
@@ -0,0 +1,60 @@
+import os
+import weave
+from pydantic import BaseModel
+from openai import OpenAI
+from weave import Model
+from dotenv import load_dotenv
+from utilities import get_prompt
+
+load_dotenv()
+
+llm_name         = os.getenv("LLM_NAME")
+
+system_prompt    = get_prompt("system_prompt")
+base_prompt      = get_prompt("base_prompt")
+
+class LabelValueOutput(BaseModel):
+    label_value_llm_output: list[float]
+
+class AGMPresenceModel(Model):
+    """Keeping these here only because it enables automatic prompt versioning in Weave.
+    Due to a deserialization issue, it is not possible to reference these as class attributes
+    further down, which is why I am falling back to using the corresponding global variables."""
+    system_prompt: weave.trace.refs.ObjectRef
+    base_prompt: weave.trace.refs.ObjectRef
+
+    @weave.op()
+    def predict(self, pages: list[str], error: str | None):
+        # TODO get metadata, specifically year, id_key, maybe unique id
+        metadata = "metadata"
+        error_message = error
+
+        client = OpenAI(
+            api_key=os.getenv("OPENAI_API_KEY")
+        )
+
+        page_analysis_results = []
+        for page in pages:
+
+            # Then try regular extraction with the header info
+            label_value_response = client.responses.parse(
+                model=llm_name, 
+                input=[
+                    {"role": "system", "content": system_prompt.get().content},
+                    {"role": "user", "content": base_prompt.get().content.format(
+                        report=page
+                    )}
+                ],
+                text_format=LabelValueOutput
+            )
+
+            page_analysis_results.extend(label_value_response.output_parsed.label_value_llm_output)
+
+        print(f"{page_analysis_results}")
+        # Get the highest value from all page analysis results
+        highest_value = max(page_analysis_results) if page_analysis_results else -2
+
+        return {'label_value_predicted': round(highest_value, 2),
+                'metadata': metadata,
+                'error': error_message,
+                }
